516a699b3d851ae5068944832a3ca166014099c6
rudycrespo <rudy.crespo20@gmail.com>
2017-08-11 13:24:08 -0400

    firebase, https

--- app/components/utils/api.js
+++ app/components/utils/api.js
@@ -13,7 +13,15 @@ module.exports = {
   },
 
   getFiveDayForecast: function(city) {
-    var encodedURI = window.encodeURI('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&type=accurate&APPID=' + apiKey + '&cnt=5');
+    var url;
+
+    if(location.protocol === 'http') {
+      url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+    } else {
+      url = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=';
+    }
+
+    var encodedURI = window.encodeURI(url + city + '&type=accurate&APPID=' + apiKey + '&cnt=5');
 
     return axios.get(encodedURI)
       .then(function(response) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var webpack = require('webpack');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,15 +24,15 @@ module.exports = {
   })]
 };
 
-// if (process.env.NODE_ENV === 'production') {
-//   config.plugins.push(
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-//       }
-//     }),
-//     new webpack.optimize.UglifyJsPlugin()
-//   )
-// }
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
 
-// module.exports = config;
+module.exports = config;
